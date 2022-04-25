a0f6de2f82cc7a1e580a963c71576e379fc8b5ef
RoyDavinci <emsthias33@gmail.com>
2022-04-25 18:31:40 +0100

    heroku build

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ import dotenv from "dotenv";
 dotenv.config();
 import airtimeRouter from "./routes/airtime.js";
 import dataRouter from "./routes/data.js";
+import path from "path";
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+// ...
+
 app.use(express.json());
 app.use(cors());
 app.use("/airtime", airtimeRouter);
@@ -13,6 +19,11 @@ app.use("/get-data", dataRouter);
 
 app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 5000;
+
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, () => {
 	console.log(`Server is running on  http://localhost:${PORT}`);
